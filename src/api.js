a5d327ec3fd708df312f720e6007eae5169b6eff
Manthri Sairam <manthrisairam@example.com>
2025-10-28 14:04:48 +0530

    railway URL added

--- src/api.js
+++ src/api.js
@@ -1,7 +1,7 @@
 // src/api.js
 const API_BASE =
   process.env.NODE_ENV === "production"
-    ? "https://YOUR-RAILWAY-APP-URL"   // e.g., https://railway.com/project/81254759-2843-4e43-a888-25ee6fec8a33
+    ? "new-production-f59b.up.railway.app"   // e.g., https://railway.com/project/81254759-2843-4e43-a888-25ee6fec8a33
     : "";                               // proxy will route to http://localhost:5000
 
 export const apiFetch = (path, options) =>
