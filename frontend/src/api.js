6664458c03e346edfe337bb1583d5ffcb18ef47c
Manthri Sairam <manthrisairam@example.com>
2025-10-28 23:27:41 +0530

    Add backend package-lock.json for Railway deployment1

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,7 +1,7 @@
 ﻿// ✅ src/api.js
 const API_BASE =
   process.env.NODE_ENV === "production"
-    ? "https://new-production-ffd2.up.railway.app"
+    ? "https://new-production-f59b.up.railway.app/"
     : "http://localhost:8080";
 
 export const apiFetch = (path, options) =>
