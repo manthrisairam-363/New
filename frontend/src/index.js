dc3d38583b01edfc1e0eb6d8e0b4089c26535696
manthrisairam-363 <75235650+manthrisairam-363@users.noreply.github.com>
2026-04-28 14:02:39 +0530

    Register service worker for PWA support
    Added service worker registration for PWA support.

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -11,7 +11,18 @@ root.render(
   </React.StrictMode>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+// Register service worker for PWA support
+if ('serviceWorker' in navigator) {
+  window.addEventListener('load', () => {
+    navigator.serviceWorker
+      .register('/sw.js')
+      .then((reg) => {
+        console.log('Service worker registered:', reg.scope);
+      })
+      .catch((err) => {
+        console.log('Service worker registration failed:', err);
+      });
+  });
+}
+
 reportWebVitals();
