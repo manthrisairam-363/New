f0d107067ceb83b78720380d80f876748b3fc073
manthrisairam-363 <75235650+manthrisairam-363@users.noreply.github.com>
2026-05-03 15:42:30 +0530

    Add Firebase Authentication to firebaseConfig

--- frontend/src/firebaseConfig.js
+++ frontend/src/firebaseConfig.js
@@ -1,6 +1,7 @@
 // src/firebaseConfig.js
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
+import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
   apiKey: "AIzaSyDywEWlitzUV5lGEfXimRTb8Y_2cSc1n0Y",
@@ -13,3 +14,4 @@ const firebaseConfig = {
 
 const app = initializeApp(firebaseConfig);
 export const db = getFirestore(app);
+export const auth = getAuth(app);
