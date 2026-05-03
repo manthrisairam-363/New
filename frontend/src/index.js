1eef630671d93c1fcd05871eb78c3fe74ea9f33e
manthrisairam-363 <75235650+manthrisairam-363@users.noreply.github.com>
2026-05-03 18:44:07 +0530

    Remove reportWebVitals import and call

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -2,7 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
@@ -24,5 +23,3 @@ if ('serviceWorker' in navigator) {
       });
   });
 }
-
-reportWebVitals();
